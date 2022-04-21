a34fb3c84636fa708e15427ef322c507c0c86746
HoangMoon <hoangmoon8321@gmail.com>
2022-04-21 12:44:59 +0700

    first commit

---
+++ Final_Exam_Sudoku/Final_Exam_Sudoku/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace Final_Exam_Sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.CheckBtn = new System.Windows.Forms.Button();
+            this.RestartBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EZ = new System.Windows.Forms.RadioButton();
+            this.NM = new System.Windows.Forms.RadioButton();
+            this.H = new System.Windows.Forms.RadioButton();
+            this.NewBtn = new System.Windows.Forms.Button();
+            this.SEZ = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(513, 468);
+            this.panel1.TabIndex = 0;
+            // 
+            // CheckBtn
+            // 
+            this.CheckBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheckBtn.Location = new System.Drawing.Point(561, 12);
+            this.CheckBtn.Name = "CheckBtn";
+            this.CheckBtn.Size = new System.Drawing.Size(114, 63);
+            this.CheckBtn.TabIndex = 1;
+            this.CheckBtn.Text = "Kiểm tra";
+            this.CheckBtn.UseVisualStyleBackColor = true;
+            this.CheckBtn.Click += new System.EventHandler(this.CheckBtn_Click);
+            // 
+            // RestartBtn
+            // 
+            this.RestartBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RestartBtn.Location = new System.Drawing.Point(561, 100);
+            this.RestartBtn.Name = "RestartBtn";
+            this.RestartBtn.Size = new System.Drawing.Size(114, 64);
+            this.RestartBtn.TabIndex = 2;
+            this.RestartBtn.Text = "Restart";
+            this.RestartBtn.UseVisualStyleBackColor = true;
+            this.RestartBtn.Click += new System.EventHandler(this.RestartBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(546, 214);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 45);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Lever:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // EZ
+            // 
+            this.EZ.AutoSize = true;
+            this.EZ.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EZ.Location = new System.Drawing.Point(561, 306);
+            this.EZ.Name = "EZ";
+            this.EZ.Size = new System.Drawing.Size(58, 29);
+            this.EZ.TabIndex = 4;
+            this.EZ.TabStop = true;
+            this.EZ.Text = "Dễ";
+            this.EZ.UseVisualStyleBackColor = true;
+            // 
+            // NM
+            // 
+            this.NM.AutoSize = true;
+            this.NM.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NM.Location = new System.Drawing.Point(561, 334);
+            this.NM.Name = "NM";
+            this.NM.Size = new System.Drawing.Size(65, 28);
+            this.NM.TabIndex = 5;
+            this.NM.TabStop = true;
+            this.NM.Text = "Vừa";
+            this.NM.UseVisualStyleBackColor = true;
+            // 
+            // H
+            // 
+            this.H.AutoSize = true;
+            this.H.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.H.Location = new System.Drawing.Point(561, 362);
+            this.H.Name = "H";
+            this.H.Size = new System.Drawing.Size(65, 28);
+            this.H.TabIndex = 6;
+            this.H.TabStop = true;
+            this.H.Text = "Khó";
+            this.H.UseVisualStyleBackColor = true;
+            // 
+            // NewBtn
+            // 
+            this.NewBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NewBtn.Location = new System.Drawing.Point(561, 408);
+            this.NewBtn.Name = "NewBtn";
+            this.NewBtn.Size = new System.Drawing.Size(114, 72);
+            this.NewBtn.TabIndex = 7;
+            this.NewBtn.Text = "Chơi Mới";
+            this.NewBtn.UseVisualStyleBackColor = true;
+            this.NewBtn.Click += new System.EventHandler(this.NewBtn_Click);
+            // 
+            // SEZ
+            // 
+            this.SEZ.AutoSize = true;
+            this.SEZ.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SEZ.Location = new System.Drawing.Point(561, 271);
+            this.SEZ.Name = "SEZ";
+            this.SEZ.Size = new System.Drawing.Size(111, 29);
+            this.SEZ.TabIndex = 8;
+            this.SEZ.TabStop = true;
+            this.SEZ.Text = "Mới Chơi";
+            this.SEZ.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 499);
+            this.Controls.Add(this.SEZ);
+            this.Controls.Add(this.NewBtn);
+            this.Controls.Add(this.H);
+            this.Controls.Add(this.NM);
+            this.Controls.Add(this.EZ);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RestartBtn);
+            this.Controls.Add(this.CheckBtn);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Sudoku-The Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button CheckBtn;
+        private System.Windows.Forms.Button RestartBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton EZ;
+        private System.Windows.Forms.RadioButton NM;
+        private System.Windows.Forms.RadioButton H;
+        private System.Windows.Forms.Button NewBtn;
+        private System.Windows.Forms.RadioButton SEZ;
+    }
+}
---
+++ Final_Exam_Sudoku/Final_Exam_Sudoku/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_Exam_Sudoku
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            createCells();
+
+            startNewGame();
+        }
+        
+
+        SudokuCell[,] cells = new SudokuCell[9, 9];
+
+        private void createCells()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    // Tạo 81 ô cho bảng với style và vị trí dựa trên tọa độ
+                    cells[i, j] = new SudokuCell();
+                    cells[i, j].Font = new Font(SystemFonts.DefaultFont.FontFamily, 20);
+                    cells[i, j].Size = new Size(40, 40);
+                    cells[i, j].ForeColor = SystemColors.ControlDarkDark;
+                    cells[i, j].Location = new Point(i * 40, j * 40);
+                    cells[i, j].BackColor = ((i / 3) + (j / 3)) % 2 == 0 ? SystemColors.Control : Color.LightGray;
+                    cells[i, j].FlatStyle = FlatStyle.Flat;
+                    cells[i, j].FlatAppearance.BorderColor = Color.Black;
+                    cells[i, j].X = i;
+                    cells[i, j].Y = j;
+
+                    // Gán sự kiện nhấn phím cho mỗi ô
+                    cells[i, j].KeyPress += cell_keyPressed;
+
+                    panel1.Controls.Add(cells[i, j]);
+                }
+            }
+        }
+
+        private void cell_keyPressed(object sender, KeyPressEventArgs e)
+        {
+            var cell = sender as SudokuCell;
+
+            // khóa ô(ko cho phép tương tác vs ô)
+            if (cell.IsLocked)
+                return;
+
+            int value;
+
+            // Thêm giá trị khi giá trị nút là number
+            if (int.TryParse(e.KeyChar.ToString(), out value))
+            {
+                // Xóa giá trị ô khi giá trị là 0
+                if (value == 0)
+                    cell.Clear();
+                else
+                    cell.Text = value.ToString();
+
+                cell.ForeColor = SystemColors.ControlDarkDark;
+            }
+        }
+        private void startNewGame()
+        {
+            loadValues();
+
+            var hintsCount = 0;
+
+            // Gán số gợi ý dựa trên lever 
+            if (SEZ.Checked)
+                hintsCount = 60;
+            else if (EZ.Checked)
+                hintsCount = 45;
+            else if (NM.Checked)
+                hintsCount = 30;
+            else if (H.Checked)
+                hintsCount = 15;
+
+            showRandomValuesHints(hintsCount);
+        }
+        
+        private void showRandomValuesHints(int hintsCount)
+        {
+            // Show  random giá trị ô 
+            // Số ô hiển thị phụ thuộc vào cấp độ của phần chơi 
+            for (int i = 0; i < hintsCount; i++)
+            {
+                var rX = random.Next(9);
+                var rY = random.Next(9);
+
+                // Style của hint khác vs giá trị nhập 
+                //  khóa giá trị ô dc hiển thị người chơi ko dc sửa  
+                cells[rX, rY].Text = cells[rX, rY].Value.ToString();
+                cells[rX, rY].ForeColor = Color.Black;
+                cells[rX, rY].IsLocked = true;
+            }
+        }
+
+        private void loadValues()
+        {
+            // Xóa giá trị từng ô
+            foreach (var cell in cells)
+            {
+                cell.Value = 0;
+                cell.Clear();
+            }
+
+            // Phương thức này sẽ được gọi đệ quy cho đến khi nó tìm thấy các giá trị phù hợp cho từng ô
+            findValueForNextCell(0, -1);
+        }
+        Random random = new Random();
+
+        private bool findValueForNextCell(int i, int j)
+        {
+            // Tăng giá trị cho i và j để chuyển sang ô tiếp theo và nếu đến cột cuối chuyển sang hàng tiếp theo
+           
+            if (++j > 8)
+            {
+                j = 0;
+
+                // Thoát khi hết dòng
+                if (++i > 8)
+                    return true;
+            }
+
+            var value = 0;
+            var numsLeft = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            //Tìm một số ngẫu nhiên và hợp lệ cho ô và chuyển đến ô tiếp theo và kiểm tra xem nó có thể được phân bổ bằng một số ngẫu nhiên và hợp lệ khác không
+            do
+            {
+                // Nếu không còn số nào trong danh sách để thử tiếp theo, hãy quay lại ô trước đó và phân bổ nó bằng một số khác
+                if (numsLeft.Count < 1)
+                {
+                    cells[i, j].Value = 0;
+                    return false;
+                }
+
+                // Lấy một số ngẫu nhiên từ các số còn lại trong danh sách
+                value = numsLeft[random.Next(0, numsLeft.Count)];
+                cells[i, j].Value = value;
+
+                // Xóa giá trị được phân bổ khỏi danh sách
+                numsLeft.Remove(value);
+            }
+            while (!isValidNumber(value, i, j) || !findValueForNextCell(i, j));
+
+            // Testing line 
+         cells[i, j].Text = value.ToString();
+
+            return true;
+        }
+
+        private bool isValidNumber(int value, int x, int y)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+
+                // Kiểm tra tất cả các ô theo hướng dọc
+                if (i != y && cells[x, i].Value == value)
+                    return false;
+
+                // Kiểm tra tất cả các ô theo hướng ngang
+                if (i != x && cells[i, y].Value == value)
+                    return false;
+            }
+
+
+            // Kiểm tra tất cả các ô trong khối cụ thể
+            for (int i = x - (x % 3); i < x - (x % 3) + 3; i++)
+            {
+                for (int j = y - (y % 3); j < y - (y % 3) + 3; j++)
+                {
+                    if (i != x && j != y && cells[i, j].Value == value)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckBtn_Click(object sender, EventArgs e)
+        {
+            var wrongCells = new List<SudokuCell>();
+
+            // Tìm tất cả ô bị sai
+            foreach (var cell in cells)
+            {
+                if (!string.Equals(cell.Value.ToString(), cell.Text))
+                {
+                    wrongCells.Add(cell);
+                }
+            }
+
+            // Kiểm tra nếu nhập đúng thỏa mãn đk thì thắng 
+            if (wrongCells.Any())
+            {
+                // Highlight lỗi sai
+                wrongCells.ForEach(x => x.ForeColor = Color.Red);
+                MessageBox.Show("Wrong inputs");
+            }
+            else
+            {
+                MessageBox.Show("You Wins");
+            }
+        }
+
+        private void RestartBtn_Click(object sender, EventArgs e)
+        {
+            foreach (var cell in cells)
+            {
+                //Xóa toàn bộ những gì đã nhập
+                if (cell.IsLocked == false)
+                    cell.Clear();
+            }
+        }
+
+        private void NewBtn_Click(object sender, EventArgs e)
+        {
+            startNewGame();
+        }
+    }
+}
